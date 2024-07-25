529ab9ed6550c2906acb0ff17da0e5106a9dd588
Stanislauf <psv88877@gmail.com>
2024-07-25 14:16:56 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+esercizi_quantita = 12
+ore_quantita = 1.5
+nome = 'Python'
+tempo_uno_esercizio = ore_quantita / esercizi_quantita
+print(f'Курс: {nome}, всего задач: {esercizi_quantita}, затрачено часов: {ore_quantita}, среднее время выполнения: {tempo_uno_esercizio}')
