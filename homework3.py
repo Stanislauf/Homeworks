976d2e3adf758ef0889697d48e9e454950620dcd
Stanislauf <psv88877@gmail.com>
2024-07-26 21:15:59 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+nome = "Stanislav"
+print('Nome: ', nome)
+era = 33
+print('Era: ', era)
+era_nuova = era + 3
+print('Era nuova: ', era_nuova)
+uno_studente = True
+print('Uno Studente: ',uno_studente)
