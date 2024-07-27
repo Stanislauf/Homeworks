47a7432a8205a078610685581b2fed41f02f13d4
Stanislauf <psv88877@gmail.com>
2024-07-27 10:39:40 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+mia_linea = input("Scrivi qualcosa: ")
+print('Lunghezza della riga:', len(mia_linea), 'caratteri')
+print(mia_linea.upper())
+print(mia_linea.lower())
+print(mia_linea.replace(' ', ''))
+print(mia_linea[0])
+print(mia_linea[-1])
