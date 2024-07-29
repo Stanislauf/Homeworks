f1e18af305c657ba471c4aba843554b79a6e43a5
Stanislauf <psv88877@gmail.com>
2024-07-29 22:54:03 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,12 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(sum(grades[0]) / len(grades[0]))
+print(sum(grades[1]) / len(grades[1]))
+print(sum(grades[2]) / len(grades[2]))
+print(sum(grades[3]) / len(grades[3]))
+print(sum(grades[4]) / len(grades[4]))
+students_list = list(students)
+print(students_list)
+
+
+
