69f8d7f08fb242d667b0ce4518cbbd51f0da634d
Stanislauf <psv88877@gmail.com>
2024-08-12 00:03:34 +0300

    module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,21 @@
 def get_multiplied_digits(number):
+    number = int(number)
     str_number = str(number)
     first = int(str_number[0])
+    while str_number.endswith('0'):
+        str_number = str_number[:-1]
     if len(str_number) > 1:
         res = first * get_multiplied_digits(int(str_number[1:]))
         return res
     else:
         return first
 
-
-print(get_multiplied_digits(20305))
+import re
+regex = '^[0-9]+$'
+number = str(input('Введите целое число: '))
+pattern = re.compile(regex)
+if pattern.search(number) is not None:
+    print(f'Произведение цифр числа {number} равно: ', get_multiplied_digits(number))
+else:
+    number = str(input('Вы ввели недопустимые символы. Введите целое число: '))
+    print(f'Произведение цифр числа {number} равно: ', get_multiplied_digits(number))
