233da170c56d05500fce033c85b730146b15074b
Stanislauf <psv88877@gmail.com>
2024-08-15 22:56:35 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def fake_divide(primo, secondo):
+    if secondo == 0:
+        return "Ошибка"
+    else:
+        res_div = primo / secondo
+        return res_div
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def true_divide(primo, secondo):
+    if secondo == 0:
+        return inf
+    else:
+        res_true = primo / secondo
+        return res_true
