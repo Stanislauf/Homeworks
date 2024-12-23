a69d8c46dc1dac6d45a9275bbcb560c5c11fbf32
Stanislauf <psv88877@gmail.com>
2024-12-23 17:22:36 +0300

    module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -1,35 +1,37 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, conint
+from fastapi import FastAPI, HTTPException, Path
 from typing import Annotated
 
 app = FastAPI()
 
 users = {'1': 'Имя: Example, возраст: 18'}
 
-class User(BaseModel):
-    username: str
-    age: conint(ge=0)
-
 @app.get("/users")
-async def get_users():
+def get_users():
     return users
 
 @app.post("/user/{username}/{age}")
-async def add_user(username: str, age: Annotated[int, conint(ge=0)]):
+def add_user(
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Введите имя пользователя")],
+    age: Annotated[int, Path(ge=18, le=120, description="Введите возраст")]
+):
     new_id = str(max(map(int, users.keys()), default=0) + 1)
     users[new_id] = f'Имя: {username}, возраст: {age}'
     return f"User {new_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, username: str, age: Annotated[int, conint(ge=0)]):
+def update_user(
+    user_id: str,
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Введите имя пользователя")],
+    age: Annotated[int, Path(ge=18, le=120, description="Введите возраст")]
+):
     if user_id in users:
         users[user_id] = f'Имя: {username}, возраст: {age}'
-        return f"The user {user_id} has been updated"
+        return f"User {user_id} has been updated"
     else:
         raise HTTPException(status_code=404, detail="User not found")
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: str):
+def delete_user(user_id: str):
     if user_id in users:
         del users[user_id]
         return f"User {user_id} has been deleted"
